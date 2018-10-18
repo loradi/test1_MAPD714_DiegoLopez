d9c5d34ac7c256934415ae8c3e7c542eea42fea0
Diego <ldiego900@gmail.com>
2018-10-18 19:00:10 -0400

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Diego  lopez on 10/18/18.
+//  Copyright © 2018 Diego_Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
